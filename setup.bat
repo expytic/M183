4ecc6febe95f081a403d40c45113fdb62a216bf1
Dominik Weber <domi-weber1@hotmail.com>
2020-05-11 11:41:16 +0200

    fixed setup.bat

--- setup.bat
+++ setup.bat
@@ -7,4 +7,4 @@ mysql.exe -u root -e "source %mypath%sql\setupdb.sql"
 mysql.exe -u root -e "source %mypath%sql\tobrunch.sql"
 mysql.exe -u root -e "source %mypath%sql\tosupport.sql"
 mysql.exe -u root -e "source %mypath%sql\users.sql"
-pause
+cd %mypath%
