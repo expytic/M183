1942668171a0a82bf98f753bb57abee232551d46
Dominik Weber <domi-weber1@hotmail.com>
2020-05-11 15:47:46 +0200

    fixed footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,17 @@
     <footer>
-    <?php
-        $kontakt = "kontakt.php";
-        if(basename(getcwd()) != 'pages') $kontakt = "pages/" . $kontakt;
-        echo "<hr><a href=\"$kontakt\">Kontakt</a>";
-    ?>
-
-        <b><p>© 2020 Doppelklang / 03.2020 BigBand Doppelklang</p></b>
-        <script src="js/bootstrap.min.js"></script>
+        <?php
+            $kontakt = "kontakt.php";
+            $bootstrapJs = "js/bootstrap.min.js";
+            if(basename(getcwd()) != 'pages'){
+                
+                $kontakt = "pages/" . $kontakt;
+            }else{
+                $bootstrapJs = "../" . $bootstrapJs;
+            }
+            echo "<hr><a href=\"$kontakt\">Kontakt</a>";
+            echo "<b><p>© 2020 Doppelklang / 03.2020 BigBand Doppelklang</p></b>";
+            echo "<script src=\"$bootstrapJs\"></script>";
+        ?>
     </footer>
 </body>
 </html>
